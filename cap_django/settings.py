c1329b9e0dd090b5c1407eac54569a9bc64e16d7
xcarrillo1 <Carrillo.Xavier@outlook.com>
2021-11-19 10:49:32 -0500

    default permission classes update

--- cap_django/settings.py
+++ cap_django/settings.py
@@ -145,8 +145,6 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
         # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
 }
